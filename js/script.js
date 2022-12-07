c83f0680099282178f558856be74b74a6f098453
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2022-12-07 16:59:28 +0100

    Aggiungo i commenti

--- js/script.js
+++ js/script.js
@@ -7,21 +7,24 @@ console.log(' JS OK');
 
 // Creo il ciclo del conteggio numeri da 1 a 100
 for (let number = 1; number <= 100; number++) {
-   
+    
+    //Calcolo i multipli di 3 e 5 e stampo FizzBuzz al posto del numero
      if (number % 3 == 0 && number % 5 == 0) {
         console.log( 'FizzBuzz');
     }
+
+    //Calcolo il multiplo di 3 e stampo Fizz al posto del numero
      else if (number % 3 == 0){
         console.log('Fizz');
     }
+
+    //Calcolo il multiplo di 5 e stampo Buzz al posto del numero
      else if (number % 5 == 0 ){
         console.log('Buzz');
     }
+
+    //Stampo tutti i restanti numeri
         else {
             console.log(number);
         }
-   
- 
-     
-
 }
